7f0c2be0507ef5d8b0ecc16c5736d40b7f32bd58
Pratiksha Avhad <pratikshaavhad91@gmail.com>
2025-01-27 23:30:16 +0530

    Add files via upload

---
+++ area2.php
@@ -0,0 +1,17 @@
+<?php
+       $radius=5;
+       $Carea= 3.14 * $radius * $radius;
+    echo("<br> Area of circle=".$Carea);
+
+  $side=4;
+  $squar=$side * $side;
+     echo("<br> Area of Squar=".$squar);
+
+   $length=6;
+   $width=3;
+     $rec=$length * $width;
+      echo("<br> Area of rectangle=".$rec);
+
+?>
+ 
+   
